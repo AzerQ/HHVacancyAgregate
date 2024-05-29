bf2e99e21be65380848e82e0b908f4a2d3115aa7
AzerQ <46249148+AzerQ@users.noreply.github.com>
2024-05-29 11:56:23 +0000

    Extend vacancy entity

---
+++ HHVacancyAnalyzer/HHVacancy.Core.Data/Models/Entities/VacacncyTypeEntity.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class VacacncyTypeEntity
+{
+        [Key]
+        [Column("VacancyTypeId")]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+}
--- HHVacancyAnalyzer/HHVacancy.Core.Data/Models/Entities/VacancyEntity.cs
+++ HHVacancyAnalyzer/HHVacancy.Core.Data/Models/Entities/VacancyEntity.cs
@@ -30,11 +30,31 @@ namespace HHVacancy.Core.Data.Models.Entities
 
         public DateTime PublishedAt { get; set; }
 
+        // TODO: Сделать преобразование в модели
         public List<string> Relations { get; set; }
 
         public bool ResponseLetterRequired { get; set; }
 
+        public string SalaryCurrency { get; set; }
 
+        public int? SalaryFrom { get; set; }
+
+        public int? SalaryTo { get; set; }
+
+        public bool? SalaryGross { get; set; }
+
+        public string SnippetRequirement { get; set; }
+
+        public string SinppetResponsibility { get; set; }
+
+        public string VacancyTypeId { get; set; }
+
+        [ForeignKey(nameof(VacancyTypeId))]
+        public VacacncyTypeEntity VacacncyType { get; set; }
+
+        public string Url { get; set; }
+
+        public bool AcceptTemporary { get; set; }
 
     }
 }
