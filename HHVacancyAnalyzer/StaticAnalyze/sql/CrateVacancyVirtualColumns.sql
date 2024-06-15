c1c5fa22634f23b73e22333a81de5112c5891e82
AzerQ <xxxvorobej71@gmail.com>
2024-06-15 13:53:05 +0300

    Add virtual columns to Vacancies table

---
+++ HHVacancyAnalyzer/StaticAnalyze/sql/CrateVacancyVirtualColumns.sql
@@ -0,0 +1,15 @@
+-- Добавление флага наличия высшего образования
+ALTER Table Vacancies
+ADD COLUMN HigherEducationMention INT GENERATED ALWAYS AS (IIF(LOWER(SnippetRequirement) LIKE '%высш__%образование%', 1, 0));
+
+-- Добавление вычисления размера начала диапозона ЗП после вычета налогов
+ALTER TABLE Vacancies
+ADD COLUMN SalaryFromClear REAL GENERATED ALWAYS AS (IIF(SalaryGross = 1, SalaryFrom * 0.87, SalaryFrom));
+
+-- Добавление вычисления размера конца диапозона ЗП после вычета налогов
+ALTER TABLE Vacancies
+ADD COLUMN SalaryToClear REAL GENERATED ALWAYS AS (IIF(SalaryGross = 1, SalaryTo * 0.87, SalaryTo));
+
+-- Добавление вычисления размера средней ЗП после вычета налогов
+ALTER TABLE Vacancies
+ADD COLUMN SalaryMiddleClear REAL GENERATED ALWAYS AS ((SalaryFromClear + SalaryToClear) / 2);
